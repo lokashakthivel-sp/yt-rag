923897dfec656aa822e6ebf6eea06dd663317e44
LokashakthivelSP <lokashakthivelsp@gmail.com>
2026-07-17 11:53:44 +0530

    fix: requirements error

--- backend/api.py
+++ backend/api.py
@@ -13,7 +13,7 @@ app = FastAPI(title="YouTube RAG API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"], 
+    allow_origins=["http://localhost:5173","https://youtuberagls.vercel.app"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
